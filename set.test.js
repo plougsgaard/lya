d8e7c8d3946322fbc4537cd76729b0dc1123d6e6
plougsgaard <thomas@plougsgaard.dk>
2019-02-24 16:44:06 +0100

    feat: `set`, `isObject` functions
    The function `set` sets a value at some path of an object (not array).

---
+++ set.test.js
@@ -0,0 +1,36 @@
+/* global describe, expect, test */
+
+const set = require('./set')
+
+describe('set top-layer keys', () => {
+  let source = { a: 1 }
+  let target = set('a', 42, source)
+  test('result has the key', () => {
+    expect(target).toEqual({ a: 42 })
+  })
+  test('does not alter source', () => {
+    expect(source).toEqual({ a: 1 })
+  })
+})
+
+describe('set nested (new) key', () => {
+  test('result has the nested key', () => {
+    let source = { a: { q: 32 } }
+    let target = set('a.b.c', 42, source)
+    expect(target).toEqual({ a: { b: { c: 42 }, q: 32 } })
+  })
+  test('does not alter source', () => {
+    let source = { a: { q: 32 } }
+    let target = set('a.b.c', 42, source)
+    target.a = 123
+    expect(source).toEqual({ a: { q: 32 } })
+  })
+})
+
+describe('negative cases', () => {
+  test('should remain unchanged', () => {
+    expect(set('0', 123, [])).toEqual([])
+    expect(set('0', 123, null)).toEqual(null)
+    expect(set('0', 123, undefined)).toEqual(undefined)
+  })
+})
