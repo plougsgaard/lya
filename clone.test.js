1a6ae5c9fcd17c1b68060802cee2bdda870d354d
plougsgaard <thomas@plougsgaard.dk>
2019-02-24 16:45:12 +0100

    test: Add some more tests for `clone`

--- clone.test.js
+++ clone.test.js
@@ -14,4 +14,16 @@ describe('clone', () => {
     expect(o1).toBe(o1)
     expect(o1).not.toBe(o2)
   })
+  test('copied object cannot alter source', () => {
+    let o1 = { a: 1 }
+    let o2 = clone(o1)
+    o2.a = 100
+    expect(o1.a).toBe(1)
+  })
+  test('nested keys still alter source', () => {
+    let o1 = { a: { b: 2 } }
+    let o2 = clone(o1)
+    o2.a.b = 100
+    expect(o1.a.b).toBe(100)
+  })
 })
