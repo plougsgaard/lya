3c3d41317de8647f97eca1b55d82c2c49806d533
plougsgaard <thomas@plougsgaard.dk>
2019-02-26 22:58:16 +0100

    feat: `reject` function
    This is just like `filter`, but throws stuff away instead.

---
+++ reject.js
@@ -0,0 +1,28 @@
+const curry2 = require('./internal/curry2')
+const makeIteratee = require('./internal/iteratee')
+const negate = require('./negate')
+
+/**
+ * Rejects elements of array by running each though iteratee.
+ *
+ * @param {string|Function} iteratee
+ * @param {Array} array
+ * @returns {Array}
+ *
+ * @example
+ * reject('a', [{ a: true }, { a: false }]) // => [{ a: false }]
+ *
+ * @example
+ * reject(v => v > 0)([-1, 0, 1, 2]) // => [-1, 0]
+ *
+ * @since 0.3.0
+ */
+function reject (iteratee, array) {
+  if (!Array.isArray(array)) {
+    return array
+  }
+  iteratee = negate(makeIteratee(iteratee))
+  return array.filter(iteratee)
+}
+
+module.exports = curry2(reject)
