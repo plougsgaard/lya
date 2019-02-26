f622fac02730cf97d3346372f952389e65675f9b
plougsgaard <thomas@plougsgaard.dk>
2019-02-26 22:57:55 +0100

    feat: `filter` function

---
+++ filter.js
@@ -0,0 +1,27 @@
+const curry2 = require('./internal/curry2')
+const makeIteratee = require('./internal/iteratee')
+
+/**
+ * Filters array (keeps elements) by iteratee (function or path).
+ *
+ * @param {string|Function} iteratee
+ * @param {Array} array
+ * @returns {Array}
+ *
+ * @example
+ * filter('a', [{ a: true }, { a: false }]) // => [{ a: true }]
+ *
+ * @example
+ * filter(v => v > 0)([-1, 0, 1, 2]) // => [1, 2]
+ *
+ * @since 0.3.0
+ */
+function filter (iteratee, array) {
+  if (!Array.isArray(array)) {
+    return array
+  }
+  iteratee = makeIteratee(iteratee)
+  return array.filter(iteratee)
+}
+
+module.exports = curry2(filter)
