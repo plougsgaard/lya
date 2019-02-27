7fd3eef31ca6164ef911eb9279ff61ca00fbc8f3
Thomas Plougsgaard <thomas@plougsgaard.dk>
2019-02-27 16:06:16 +0100

    feat: Expand `clone` function to clone arrays (#22)
    * feat: Expand `clone` function to clone arrays
    
    I suspect this is going to be a theme going forward.
    
    * docs: Generate README

--- clone.js
+++ clone.js
@@ -1,19 +1,27 @@
 const keys = require('./keys')
+const slice = require('./slice')
 
 /**
  * Clone (shallow) collection.
  *
- * @param {Object} collection - Collection to clone
- * @returns {Object} shallow clone of collection
+ * @param {Object|Array} collection - Collection to clone
+ * @returns {Object|Array} shallow clone of collection
  *
  * @example
  * let obj = { a: 1 }
- * let c = clone(obj) // => c = { a: 1 }, obj != c
+ * let res = clone(obj) // => res = { a: 1 }, obj != res
  *
- * @since 0.2.0
+ * @example
+ * let arr = [1, 2, 3]
+ * let res = clone(arr) // => res = [1, 2, 3], obj != res
+ *
+ * @since 0.2.0 - array clones introduced in 0.3.0
  */
 function clone (collection) {
-  let result = {}
+  if (Array.isArray(collection)) {
+    return slice(0, undefined, collection)
+  }
+  var result = {}
   for (var k of keys(collection)) {
     result[k] = collection[k]
   }
