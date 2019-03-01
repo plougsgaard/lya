51d5605bb51572ce57ed2caeac7c79656ed8bd8b
plougsgaard <thomas@plougsgaard.dk>
2019-03-01 23:43:56 +0100

    fix: Typo - last, not sast

--- last.js
+++ last.js
@@ -6,7 +6,7 @@ var nth = require('./nth')
  * @function
  *
  * @param {Array} array - array to get last element from
- * @returns {*} Return sast element of `array`
+ * @returns {*} Return last element of `array`
  *
  * @example
  * last([1, 2, 3]) // => 3
