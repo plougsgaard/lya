b4013a994fe567b2762abd7fac8411b62f796257
plougsgaard <thomas@plougsgaard.dk>
2019-03-02 13:28:45 +0100

    feat: Wrapper to call string prototype function

---
+++ internal/callWhenString.js
@@ -0,0 +1,21 @@
+var values = require('../values')
+
+function callWhenString (predicate, arity, defaultValue) {
+  return function () {
+    var string = arguments[arity - 1]
+    if (typeof string !== 'string') {
+      return defaultValue || string
+    }
+    if (arity === 1) {
+      return predicate.call(arguments[0])
+    } else if (arity === 2) {
+      return predicate.call(arguments[1], arguments[0])
+    } else if (arity === 3) {
+      return predicate.call(arguments[2], arguments[0], arguments[1])
+    } else {
+      return defaultValue
+    }
+  }
+}
+
+module.exports = callWhenString
