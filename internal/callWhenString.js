f15ea8529a94f5d202d771dd73da32545b834a6e
plougsgaard <thomas@plougsgaard.dk>
2019-03-02 15:32:41 +0100

    refactor: Use string variable directly
    It's also bound, so why not.

--- internal/callWhenString.js
+++ internal/callWhenString.js
@@ -5,9 +5,9 @@ function callWhenString (predicate, arity, defaultValue) {
       return defaultValue || string
     }
     switch (arity) {
-      case 1: return predicate.call(arguments[0])
-      case 2: return predicate.call(arguments[1], arguments[0])
-      case 3: return predicate.call(arguments[2], arguments[0], arguments[1])
+      case 1: return predicate.call(string)
+      case 2: return predicate.call(string, arguments[0])
+      case 3: return predicate.call(string, arguments[0], arguments[1])
       default: throw new Error('Internal function `callWhenString` used improperly.')
     }
   }
