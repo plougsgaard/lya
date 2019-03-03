8fd44fe0a6df473ad15226aa8653da4475c24136
plougsgaard <thomas@plougsgaard.dk>
2019-03-03 12:11:20 +0100

    test: Add additional test to demonstrate inconsistent API
    If concat's second argument isn't an array it just gets treated as if it were anyway.
    So if you want a nested array you need to wrap it twice.

--- tests/concat.test.js
+++ tests/concat.test.js
@@ -9,14 +9,17 @@ describe('concat', () => {
   test('concat array', () => {
     expect(_.concat([3, 4], [1, 2])).toEqual([1, 2, 3, 4])
   })
+  test('concat array of array', () => {
+    expect(_.concat([[3, 4]], [1, 2])).toEqual([1, 2, [3, 4]])
+  })
   test('concat strings', () => {
     expect(_.concat('1', '2')).toEqual('21')
+  })
+  test('concat array with string', () => {
+    expect(_.concat('1', ['2'])).toEqual(['2', '1'])
   })
   test('concat string with non-strings', () => {
     expect(_.concat({}, 'str')).toEqual('str[object Object]')
     expect(_.concat(undefined, 'str')).toEqual('strundefined')
-  })
-  test('concat array with string', () => {
-    expect(_.concat('1', ['2'])).toEqual(['2', '1'])
   })
 })
