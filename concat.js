b3ca9b77f3deb6f43c3bacae5253564edd244414
plougsgaard <thomas@plougsgaard.dk>
2019-03-03 11:49:57 +0100

    feat: `concat` now also concats strings

--- concat.js
+++ concat.js
@@ -1,11 +1,11 @@
 var curry2 = require('./internal/curry2')
 
 /**
- * Append value or array to array
+ * Append value or array to array, or value to string
  *
- * @param {*|Array} value - value or array to append to array
- * @param {*|Array} array - value or array to expand
- * @returns {Array} Returns concatenated array
+ * @param {Array|*} value - value or array to append to array or string
+ * @param {Array|string} array - string or array to expand
+ * @returns {Array|string} Returns concatenated array or string
  *
  * @see https://mdn.io/concat
  *
@@ -15,13 +15,20 @@ var curry2 = require('./internal/curry2')
  * @example
  * concat([4, 5], [1, 2, 3]) // => [1, 2, 3, 4, 5]
  *
+ * @example
+ * concat('def', 'abc') // => 'abcdef'
+ *
+ * @example
+ * concat('def', ['abc']) // => ['abc', 'def']
+ *
  * @since 0.4.0
  */
 function concat (value, array) {
-  if (!Array.isArray(array)) {
-    array = [array]
+  if (Array.isArray(array) || typeof array === 'string') {
+    return array.concat(value)
+  } else {
+    return array
   }
-  return array.concat(value)
 }
 
 module.exports = curry2(concat)
